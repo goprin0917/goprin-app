38c80938d67ace61a04aacd888169866e114ce1b
Gabby Oprin <gabrieloprin@gmail.com>
2025-02-27 18:15:21 +0800

    Implementing sidebar menu on click

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+document.querySelector(".email-anchor")?.addEventListener("click", (event) => {
+  event.preventDefault();
+  openEmailClient();
+});
 function openEmailClient() {
   const email = "gabrieloprin@gmail.com";
   const mailtoLink = `mailto:${email}`;
   window.open(mailtoLink, "_blank");
 }
+
+document
+  .querySelector(".linkedin-anchor")
+  ?.addEventListener("click", (event) => {
+    event.preventDefault();
+    openLinkedIn();
+  });
 
 function openLinkedIn() {
   const url = "https://www.linkedin.com/in/gabriel-oprin-16b002245";
@@ -12,7 +25,7 @@ function openLinkedIn() {
 const downloadResume = async () => {
   try {
     const response = await fetch(
-      "https://oxbvyasg79.execute-api.ap-southeast-1.amazonaws.com/default/downloadResume"
+      `${import.meta.env.VITE_AWS_API_GATEWAY_BASE_URL}/default/downloadResume`
     );
     const data = await response.json();
 
@@ -36,7 +49,15 @@ const downloadResume = async () => {
   }
 };
 
+document
+  .querySelector(".download-anchor")
+  ?.addEventListener("click", (event) => {
+    event.preventDefault();
+    downloadResume();
+  });
+
 document.addEventListener("scroll", (event) => {
+  event.preventDefault();
   let lastScrollTop = 0;
   const currentScrollTop = window.scrollY;
 
@@ -52,6 +73,7 @@ document.addEventListener("scroll", (event) => {
 });
 
 document.addEventListener("scroll", (event) => {
+  event.preventDefault();
   const footer = document.getElementById("footerContentWrapper");
   const scrollPosition = window.scrollY + window.innerHeight;
   const documentHeight = document.documentElement.scrollHeight;
@@ -81,7 +103,27 @@ const observer = new IntersectionObserver(
 const hiddenElements = document.querySelectorAll(".hide");
 hiddenElements.forEach((element) => observer.observe(element));
 
-function scrollToElement(elementId) {
+document.getElementById("homeMarker")?.addEventListener("click", (event) => {
+  event.preventDefault();
+  scrollToElement("home");
+});
+
+document.getElementById("homeNavLabel")?.addEventListener("click", (event) => {
+  event.preventDefault();
+  scrollToElement("home");
+});
+
+document.getElementById("aboutMarker")?.addEventListener("click", (event) => {
+  event.preventDefault();
+  scrollToElement("about");
+});
+
+document.getElementById("aboutNavLabel")?.addEventListener("click", (event) => {
+  event.preventDefault();
+  scrollToElement("about");
+});
+
+function scrollToElement(elementId: string) {
   const element = document.getElementById(elementId);
   if (element) {
     element.scrollIntoView({ behavior: "smooth" });
@@ -120,14 +162,16 @@ carouselItems[currentIndex].classList.add("active");
 setInterval(showNextItem, 20000);
 
 const copyrightYearLabelElement = document.querySelectorAll("#copyrightYear");
+const year = new Date().getFullYear();
 copyrightYearLabelElement.forEach((element) => {
-  element.textContent = new Date().getFullYear();
+  element.textContent = `${year}`;
 });
 
 const aboutDescriptionContainerElement = document.getElementById(
   "aboutDescriptionContainer"
 );
-const educationContainerElement = document.getElementById("educationContainer");
+const educationContainerElement =
+  document.getElementById("educationContainer")!;
 const hobbiesContainerElement = document.getElementById("hobbiesContainer");
 
 const educationImageCarouselElement = document.getElementById(
@@ -141,11 +185,11 @@ const educationContainerAndParentContainerObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        educationImageCarouselElement.classList.add(
+        educationImageCarouselElement?.classList.add(
           "show-about-image-containers"
         );
       } else {
-        educationImageCarouselElement.classList.remove(
+        educationImageCarouselElement?.classList.remove(
           "show-about-image-containers"
         );
       }
@@ -179,3 +223,22 @@ educationContainerAndParentContainerObserver.observe(educationContainerElement);
 // );
 
 // hobbiesContainerAndParentContainerObserver.observe(hobbiesContainerElement);
+
+//TODO: Fix change of icon on click
+
+let isMenuClicked = false;
+
+const barsIconMenuElement = document.getElementById("barsIconMenu")!;
+barsIconMenuElement.setAttribute(
+  "src",
+  isMenuClicked ? "/assets/bars-solid.svg" : "/assets/xmark-solid.svg"
+);
+
+barsIconMenuElement?.addEventListener("click", (event) => {
+  event.preventDefault();
+  handleMenuClick();
+});
+
+function handleMenuClick() {
+  isMenuClicked = !isMenuClicked;
+}
