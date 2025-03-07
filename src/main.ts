0fba48c123e696337c7afa589a5c733382d279f4
Gabriel Oprin <gabrieloprin@gmail.com>
2025-03-07 20:23:57 +0800

    Added caching in fetching the github endpoint. Added dynamic project update status.

--- src/main.ts
+++ src/main.ts
@@ -249,37 +249,11 @@ const aboutDescriptionContainerElement = document.getElementById(
   "aboutDescriptionContainer"
 );
 
-const aboutIntroContainerElement = document.getElementById(
-  "aboutIntroContainer"
-)!;
 const educationContainerElement =
   document.getElementById("educationContainer")!;
 
-const aboutIntroImageContainerElement = document.getElementById(
-  "aboutIntroImageContainer"
-);
 const educationImageCarouselElement = document.getElementById(
   "educationImageCarousel"
-);
-
-const aboutIntroContainerAndParentContainerObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        aboutIntroImageContainerElement?.classList.add(
-          "hide-about-image-containers"
-        );
-      } else {
-        aboutIntroImageContainerElement?.classList.remove(
-          "hide-about-image-containers"
-        );
-      }
-    });
-  },
-  {
-    root: aboutDescriptionContainerElement,
-    threshold: 0.2,
-  }
 );
 
 const educationContainerAndParentContainerObserver = new IntersectionObserver(
@@ -302,9 +276,6 @@ const educationContainerAndParentContainerObserver = new IntersectionObserver(
   }
 );
 
-aboutIntroContainerAndParentContainerObserver.observe(
-  aboutIntroContainerElement
-);
 educationContainerAndParentContainerObserver.observe(educationContainerElement);
 
 let isMenuActive = false;
@@ -313,10 +284,20 @@ const barsIconMenuElement = document.getElementById("barsIconMenu")!;
 const headerContainerElement = document.getElementById("headerContainer");
 
 function updateMenu() {
-  barsIconMenuElement.setAttribute(
-    "src",
-    isMenuActive ? "/assets/xmark-solid.svg" : "/assets/bars-solid.svg"
-  );
+  switch (isMenuActive) {
+    case true:
+      barsIconMenuElement?.classList.add("nf-oct-x")
+      barsIconMenuElement?.classList.remove("nf-fa-bars")
+
+      break;
+    case false:
+      barsIconMenuElement?.classList.add("nf-fa-bars")
+      barsIconMenuElement?.classList.remove("nf-oct-x")
+      break;
+    default:
+      break;
+  }
+
 
   if (isMenuActive) {
     headerContainerElement?.classList.add("show-header-container");
@@ -351,94 +332,159 @@ const languageColors: LanguageColors = {
   PHP: "#4F5D95",
 };
 
+const CACHE_KEY = "github_languages";
+const CACHE_EXPIRY = 60 * 60 * 1000;
+
 const fetchRepositoryLanguages = async (
   repoUrl: string,
   progressBarClass: string,
   labelContainerClass: string
 ) => {
   try {
-    const response = await fetch(repoUrl);
 
-    if (!response.ok) {
-      throw new Error(`HTTP Error! status: ${response.status}`);
-    }
+    const cachedData = localStorage.getItem(CACHE_KEY);
+    const now = Date.now();
 
-    const data = (await response.json()) as Record<string, number>;
+    if (cachedData) {
+      const { timestamp, data, etag } = JSON.parse(cachedData);
 
-    // This will calculate the total bytes of the languages in the project.
-    const totalBytes = Object.values(data).reduce(
-      (sum: number, bytes: number) => sum + bytes,
-      0
-    );
+      if (now - timestamp < CACHE_EXPIRY) {
+        renderLanguages(data, progressBarClass, labelContainerClass);
+        return;
+      }
 
-    // Get the progress bar element
-    const progressBar = document.querySelector(
-      `.${progressBarClass}`
-    ) as HTMLElement;
-    if (!progressBar) return;
+      // This will fetch new data with the cached ETag.
+      const response = await fetch(repoUrl, {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          "If-None-Match": etag
+        }
+      });
 
-    // Get the language label container element
-    const labelContainer = document.querySelector(
-      `.${labelContainerClass}`
-    ) as HTMLElement;
-    if (!labelContainer) return;
+      if (response.status === 304) {
+        renderLanguages(data, progressBarClass, labelContainerClass);
+        return;
+      }
 
-    // Clear any existing items
-    progressBar.innerHTML = "";
-    labelContainer.innerHTML = "";
+      if (!response.ok) {
+        throw new Error(`HTTP Error! status: ${response.status}`);
+      }
 
-    // This will track the current position for placing items.
-    let currentPosition = 0;
+      const newData = await response.json();
+      const newEtag = response.headers.get("ETag");
 
-    // Create a progress items for each language.
-    for (const [language, bytes] of Object.entries(data)) {
-      const percentage = (bytes / totalBytes) * 100;
-      const formattedPercentage = percentage.toFixed(1) + "%";
+      // This will update the cache in localStorage
+      localStorage.setItem(
+        CACHE_KEY,
+        JSON.stringify({ timestamp: now, data: newData, etag: newEtag })
+      );
+      renderLanguages(data, progressBarClass, labelContainerClass);
+    } else {
+      const response = await fetch(repoUrl, {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
+        }
+      });
 
-      // This will create new progress item.
-      const progressItem = document.createElement("div");
-      progressItem.className = "progress-item";
-      // This will set the position and width of the progress item
-      progressItem.style.left = `${currentPosition}%`;
-      progressItem.style.width = `${percentage}%`;
+      if (!response.ok) {
+        throw new Error(`HTTP Error! status: ${response.status}`);
+      }
 
-      // This will set the color of the progress item (fallback to gray if language do not exist in mapping)
-      const color = languageColors[language] || "#ddd";
-      progressItem.style.backgroundColor = color;
+      const data = await response.json();
+      const etag = response.headers.get("ETag");
 
-      // This will add the progress item to progress bar
-      progressBar.appendChild(progressItem);
+      localStorage.setItem(
+        CACHE_KEY,
+        JSON.stringify({
+          timestamp: now,
+          data,
+          etag
+        })
+      );
 
-      // This will create a language label element
-      const labelInnerContainer = document.createElement("div");
-      labelInnerContainer.className = "language-label-inner-container";
-
-      //This will create a circle marker element for label.
-      const labelCircleMarker = document.createElement("div");
-      labelCircleMarker.className = "language-circle-marker";
-      labelCircleMarker.style.backgroundColor = color;
-
-      // This will create a language name label element
-      const languageLabel = document.createElement("label");
-      languageLabel.className = "language-label";
-      languageLabel.textContent = language;
-
-      // This will create a percentage span element
-      const percentSpan = document.createElement("span");
-      percentSpan.className = "progress-percent";
-      percentSpan.textContent = formattedPercentage;
-
-      // This will assemble the label container.
-      labelInnerContainer.appendChild(labelCircleMarker);
-      labelInnerContainer.appendChild(languageLabel);
-      labelInnerContainer.appendChild(percentSpan);
-      labelContainer.appendChild(labelInnerContainer);
-
-      // Update position for next itme.
-      currentPosition += percentage;
+      renderLanguages(data, progressBarClass, labelContainerClass)
     }
   } catch (error) {
     console.error("Error fetching language data:", error);
+  }
+};
+
+const renderLanguages = (
+  data: Record<string, number>,
+  progressBarClass: string,
+  labelContainerClass: string
+) => {
+  // This will calculate the total bytes of the languages in the project.
+  const totalBytes = Object.values(data).reduce(
+    (sum: number, bytes: number) => sum + bytes,
+    0
+  );
+
+  // Get the progress bar element
+  const progressBar = document.querySelector(
+    `.${progressBarClass}`
+  ) as HTMLElement;
+  if (!progressBar) return;
+
+  // Get the language label container element
+  const labelContainer = document.querySelector(
+    `.${labelContainerClass}`
+  ) as HTMLElement;
+  if (!labelContainer) return;
+
+  // Clear any existing items
+  progressBar.innerHTML = "";
+  labelContainer.innerHTML = "";
+
+  // This will track the current position for placing items.
+  let currentPosition = 0;
+
+  // Create a progress items for each language.
+  for (const [language, bytes] of Object.entries(data)) {
+    const percentage = (bytes / totalBytes) * 100;
+    const formattedPercentage = percentage.toFixed(1) + "%";
+
+    // This will create new progress item.
+    const progressItem = document.createElement("div");
+    progressItem.className = "progress-item";
+    // This will set the position and width of the progress item
+    progressItem.style.left = `${currentPosition}%`;
+    progressItem.style.width = `${percentage}%`;
+
+    // This will set the color of the progress item (fallback to gray if language do not exist in mapping)
+    const color = languageColors[language] || "#ddd";
+    progressItem.style.backgroundColor = color;
+
+    // This will add the progress item to progress bar
+    progressBar.appendChild(progressItem);
+
+    // This will create a language label element
+    const labelInnerContainer = document.createElement("div");
+    labelInnerContainer.className = "language-label-inner-container";
+
+    //This will create a circle marker element for label.
+    const labelCircleMarker = document.createElement("div");
+    labelCircleMarker.className = "language-circle-marker";
+    labelCircleMarker.style.backgroundColor = color;
+
+    // This will create a language name label element
+    const languageLabel = document.createElement("label");
+    languageLabel.className = "language-label";
+    languageLabel.textContent = language;
+
+    // This will create a percentage span element
+    const percentSpan = document.createElement("span");
+    percentSpan.className = "progress-percent";
+    percentSpan.textContent = formattedPercentage;
+
+    // This will assemble the label container.
+    labelInnerContainer.appendChild(labelCircleMarker);
+    labelInnerContainer.appendChild(languageLabel);
+    labelInnerContainer.appendChild(percentSpan);
+    labelContainer.appendChild(labelInnerContainer);
+
+    // Update position for next itme.
+    currentPosition += percentage;
   }
 };
 
@@ -446,4 +492,83 @@ fetchRepositoryLanguages(
   "https://api.github.com/repos/goprin0917/goprin-app/languages",
   "progress-bar",
   "language-label-container"
+);
+
+interface StatusOptions {
+  name: string;
+  value: string;
+  iconClass: string;
+  color: string;
+}
+
+const statusOptions: StatusOptions[] = [
+  {
+    name: "In progress",
+    value: "in-progress",
+    iconClass: "nf-md-progress_clock",
+    color: "#328fd4"
+  },
+  {
+    name: "Completed",
+    value: "completed",
+    iconClass: "nf-md-progress_check",
+    color: "#32c054"
+  },
+  {
+    name: "On Pause",
+    value: "on-pause",
+    iconClass: "nf-md-progress_check",
+    color: "#818181"
+  },
+  {
+    name: "Cancelled",
+    value: "cancelled",
+    iconClass: "nf-md-progress_close",
+    color: "#de3838"
+  }
+]
+
+function updateProjectStatus(
+  statusIconId: string,
+  statusValueId: string,
+  statusLabelValue: string,
+  iconClassValue: string,
+  textColor: string,
+) {
+  const statusIconElement = document.getElementById(statusIconId);
+  const statusValueElement = document.getElementById(statusValueId)!;
+
+  if (statusIconElement) {
+    statusIconElement.className = "";
+    statusIconElement.classList.add("nf");
+    statusIconElement.classList.add(iconClassValue);
+  }
+
+  statusValueElement.textContent = statusLabelValue;
+  statusValueElement.style.color = textColor
+
+}
+
+function updateToStatus(
+  statusIconId: string,
+  statusValueId: string,
+  statusValue: string
+) {
+  const selectedStatus = statusOptions.find(status => status.value === statusValue);
+
+  if (selectedStatus) {
+    updateProjectStatus(
+      statusIconId,
+      statusValueId,
+      selectedStatus.name,
+      selectedStatus.iconClass,
+      selectedStatus.color
+    )
+  }
+}
+
+updateToStatus(
+  "portfolioProjectStatusIcon",
+  "portfolioProjectStatusValue",
+  "in-progress"
 );
